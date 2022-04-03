15800e9e9abe40f5a9b97da047ca04e6de4e788f
tonyliang19 <chunqingliang@gmail.com>
2022-04-03 12:25:12 -0700

    fixing makefile and 1st script

--- src/download_data.R
+++ src/download_data.R
@@ -3,11 +3,12 @@
 
 "This script downloads the data from the internet and saves it locally.
 
-Usage: download_data.R --url=<url> --file_path=<file_path> 
+Usage: download_data.R --url=<url> --file_path=<file_path> --file_name=<file_name>
 
 Options:
     --url=<url>               Url to download data
     --file_path=<file_path>   Path to the data file
+    --file_name=<file_name>   Name to call the file
     " -> doc
 
 library(tidyverse)
@@ -15,8 +16,15 @@ library(docopt)
 
 opt <- docopt(doc)
 
-data <- download.file(opt$url,opt$file_path)
+main <- function(url, file_path, file_name) {
+if (!dir.exists(file_path)) {
+    dir.create(file_path)
+}    
 
-print(data)
-print(opt)
+data <- download.file(url,paste0(file_path, file_name))
+
+}
+
+main(opt[["--url"]], opt[["--file_path"]], opt[["--file_name"]])
+
 
